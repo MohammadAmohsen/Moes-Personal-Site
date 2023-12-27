52ba66609ba47d476a3d1993751bab7729e95024
MohammadAmohsen <moeabd99@outlook.com>
2023-12-27 16:42:53 -0500

    fixed pic error

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="/images/BissandMoe.jpg.jpg" alt="" />
+               <img className="profile-pic"  src="images/BissandMoe.jpg.jpg" alt="" />
 
             </div>
 
